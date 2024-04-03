4d0fd5d35d8f98500c906ddd6bc3fc4586fdfdfe
KV <kvid@users.noreply.github.com>
2024-04-03 10:16:05 +0200

    Move common code to separate file
    Prepare easier support for other protocols by moving common code
    to a common file that is imported from each different protocol files.

--- XInTheLoop/Resources/tools/site1-protocol.py
+++ XInTheLoop/Resources/tools/site1-protocol.py
@@ -1,5 +1,5 @@
 # Receive or send Site1 protocol messages via UDP
-# Copyright (c) 2021-2023 Virtual-FCS H2020
+# Copyright (c) 2021-2024 Virtual-FCS H2020
 # Current Site1 outgoing protocol message (out from Modelica) contains:
 # - Magic number as 4 bytes
 # - Version number as 4 bytes unsigned integer
@@ -33,71 +33,23 @@
 #  Optional: Vector of delta values between each message in a series (default +1)
 # Any missing values in the vectors are assumed to be 1 (default value)
 
-from pathlib import Path
-from socket import socket, AF_INET, SOCK_DGRAM
-from struct import pack, unpack
-from sys import argv
-from time import sleep
+from xil.common import Config, Protocol
 
-HOST = 'localhost'
+site1 = Protocol(
+  oconfig = Config(
+    port = 10002,
+    magic = 0x53434656, # Little endian b'VFCS'
+    ver = 3,
+    format = '<2L2H1L3f',
+    types = (int,) + 3*(float,),
+  ),
+  iconfig = Config(
+    port = 10001,
+    magic = 0x73636676, # Little endian b'vfcs'
+    ver = 3,
+    format = '<2L2H2B1H22f',
+    types = 3*(int,) + 22*(float,)
+  ),
+)
 
-# Tuple of outgoing (index=0) and incoming (index=1) values
-PORT = (10002, 10001)
-FORMAT = ('<2L2H1L3f', '<2L2H2B1H22f')
-MAGIC = (0x53434656, 0x73636676)  # Little endian (b'VFCS', b'vfcs')
-VER = (3, 3)
-TYPES = ((int,) + 3*(float,), 3*(int,) + 22*(float,))
-
-fname = Path(argv[0])
-fname = fname.parent / ('_' + fname.name)
-
-def savefile(direction : int, send_or_receive : int) -> Path:
-  """Return Path object storing the last package."""
-  return fname.with_suffix(f'.${direction}{send_or_receive}')
-
-def savefile_seq(direction : int, send_or_receive : int) -> int:
-  """Return sequence number of the last package or 0 if none."""
-  try:
-    return unpack(FORMAT[direction], savefile(direction, send_or_receive).read_bytes())[2]
-  except:
-    return 0
-
-direction = argv[1] if len(argv) > 1 else 'i'
-if not direction in ('i', 'in', 'o', 'out'):
-  raise Exception(f'Usage: python3 {argv[0]} [[in|out] [payload vector [count [delta vector]]]]')
-index = 1 if direction[0] == 'i' else 0
-
-if len(argv) < 3:
-  # Receiving packets
-  with socket(AF_INET, SOCK_DGRAM) as s:
-    s.bind((HOST, PORT[index]))
-    while True:
-      print(f"Listening for UDP package at {HOST}:{PORT[index]}")
-      package, address = s.recvfrom(1024)
-      print(f"Received {package.hex()} from {':'.join(str(e) for e in address)}")
-      values = unpack(FORMAT[index], package)
-      print(f"Unpacked values {values}")
-      if values[0] == MAGIC[index] and values[1] == VER[index]:
-        savefile(index, 1).write_bytes(package)
-        print(f"seq=({values[2]}, {values[3]}), {values[4:]}")
-      else:
-        print("Error: Unknown magic number and/or version in header received!")
-
-# Sending packets
-
-values = tuple(t(argv[2+i] if 2+i < len(argv) else 1) for i, t in enumerate(TYPES[index] + (int,) + TYPES[index]))
-median = len(values) // 2
-n = values[median]
-delta = values[median + 1:]
-values = values[:median]
-with socket(AF_INET, SOCK_DGRAM) as s:
-  s.connect((HOST, PORT[index]))
-  for i in range(n):
-    vector = tuple(v + i * d for v, d in zip(values, delta))
-    seq = 1 + savefile_seq(index, 0)  # Last sent seq in same direction.
-    seq_rev = savefile_seq(1 - index, 1)  # Last received seq in reverse direction.
-    package = pack(FORMAT[index], MAGIC[index], VER[index], seq, seq_rev, *vector)
-    print(f'Sending({seq}, {seq_rev}) {vector} as {package.hex()} to {HOST}:{PORT[index]}')
-    s.sendall(package)
-    savefile(index, 0).write_bytes(package)
-    sleep(1)
+site1.run()
