3501ea5bad536c00363b08c4bef6ca5574650069
KV <kvid@users.noreply.github.com>
2024-10-02 20:34:35 +0200

    Improve FCCU setpoint mode simulation for more realistic usage

--- XInTheLoop/Resources/tools/site2-fccu.py
+++ XInTheLoop/Resources/tools/site2-fccu.py
@@ -5,6 +5,7 @@ import can
 import cantools
 import keyboard
 from pathlib import Path
+from timeit import default_timer as timer
 from stashh.incoming import IncomingCanService
 
 if __name__ == "__main__":
@@ -16,11 +17,31 @@ if __name__ == "__main__":
     print("Create StasHH incoming CAN service...")
     canService = IncomingCanService(bus, db, tscale=1)
 
+    prev = 0
+    t0 = 0
+
+    def translate_mode(req: int) -> int:
+      """Simulate returned mode from requested setpoint mode"""
+      global prev
+      global t0
+      deviation = {  # Simulate the deviation from requested to returned setpoint mode
+        10: 18,
+        21: 20,
+      }
+      if req != prev:  # New mode requested?
+        t0 = timer()
+        prev = req
+      if req == 10 and (timer() - t0) > 20:  # Simulate starting takes 20 seconds
+        req = 0
+      if req == 21 and (timer() - t0) > 2:  # Simulate stopping takes 2 seconds
+        req = 2
+      return deviation.get(req, req)
+
     def received(msg: can.Message) -> None:
       """Received CAN message, but don't print it"""
       # Simulate incoming values based on outgoing requests.
       canService.dict_encode({
-        "MG1IMF1_Setpoint_Mode": canService.received_signals.get("MG1IC_Setpoint_Mode_Req", 0),
+        "MG1IMF1_Setpoint_Mode": translate_mode(canService.received_signals.get("MG1IC_Setpoint_Mode_Req", 0)),
         "MG1IMF1_HVIL_Status": canService.received_signals.get("HVBI_Driveline_Availability", 0) == 1,
         "MG1IS1_DC_current_output": canService.received_signals.get("MG1IC_Setpoint_Req", 0),
         "MG1IS1_DC_voltage_output": canService.received_signals.get("MG1IC_Setpoint_Req", 0),
