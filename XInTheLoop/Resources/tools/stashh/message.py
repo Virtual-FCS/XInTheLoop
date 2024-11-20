41973f3c04dde55fa5940d8aa9ac36af9d66bca6
kvid <kvid@users.noreply.github.com>
2024-11-20 01:39:09 +0000

    Revert "Ignore empty messages"
    This reverts commit 17be6fb8772c822471f82fcc585d6d0d627e8a7e.

--- XInTheLoop/Resources/tools/stashh/message.py
+++ XInTheLoop/Resources/tools/stashh/message.py
@@ -116,9 +116,6 @@ class CanService:
 
     def received(msg: can.Message) -> None:
       """Received CAN message"""
-      if msg is None:
-        print("Ignoring empty message received")
-        return
       try:
         signals = self.db.decode_message(msg.arbitration_id, msg.data, decode_choices=False)
         self.received_signals.update(signals)
